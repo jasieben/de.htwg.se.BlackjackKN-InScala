97e761508792d675641b2a298b98ae936701e2b3
jasieben <jana.sieben@aol.de>
2019-03-28 17:07:30 +0100

    SCR

---
+++ src/main/scala/de/htwg/se/BlackjackKN/BlackjackKN.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.BlackjackKN
+
+import de.htwg.se.BlackjackKN.model.Player
+
+object BlackjackKN {
+  def main(args: Array[String]): Unit = {
+    val student = Player("Your Name")
+    println("Hello, " + student.name)
+  }
+}
