b4d1479918b5b21322e029629ef998e7cc16688b
benjasper <jasper.benni@gmail.com>
2019-03-29 11:33:51 +0100

    added another println

--- src/main/scala/de/htwg/se/BlackjackKN/BlackjackKN.scala
+++ src/main/scala/de/htwg/se/BlackjackKN/BlackjackKN.scala
@@ -5,6 +5,7 @@ import de.htwg.se.BlackjackKN.model.Player
 object BlackjackKN {
   def main(args: Array[String]): Unit = {
     val student = Player("Benni und Jana")
-    println("Hello, " + student.name)
+    println("Hello, " + student.name + "!")
+    println("Ich bin das Spiel Blackjack!")
   }
 }
