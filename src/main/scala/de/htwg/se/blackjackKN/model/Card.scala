bc9d45614e3e9bdb21e2539960c67a9522b94a1e
benjasper <jasper.benni@gmail.com>
2019-04-12 12:16:16 +0200

    splitted Card Classes

--- src/main/scala/de/htwg/se/blackjackKN/model/Card.scala
+++ src/main/scala/de/htwg/se/blackjackKN/model/Card.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.blackjackKN.model
 
-case class Card(suit : String, value: Int, rank : String) {
-  override def toString:String = rank + " of " + suit
-
+trait Card {
+  def suit : String
+  def value: Int
 }
---
+++ src/main/scala/de/htwg/se/blackjackKN/model/FaceCard.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.blackjackKN.model
+
+class FaceCard {
+
+}
