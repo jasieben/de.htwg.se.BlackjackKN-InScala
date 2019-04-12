021aa7222568b55655ec7240a6dab6b34d7a3f27
Benjamin Jasper <jasper.benni@gmail.com>
2019-04-12 17:46:46 +0200

    implemented Dealer generating 312 Cards

--- src/main/scala/de/htwg/se/blackjackKN/aview/Tui.scala
+++ src/main/scala/de/htwg/se/blackjackKN/aview/Tui.scala
@@ -1,12 +1,14 @@
 package de.htwg.se.blackjackKN.aview
 
-import de.htwg.se.blackjackKN.model.{CardDeck, FaceCard}
+import de.htwg.se.blackjackKN.model.{CardDeck, Dealer}
 
 class Tui {
   def processInput(input: String): Unit = {
+    val dealer = Dealer()
     input match {
       case "n" =>
         println("Started a new game!")
+        println(dealer.generateDealerCards.size)
       case "q" =>
         println("Exiting Blackjack...")
         System.exit(0)
--- src/main/scala/de/htwg/se/blackjackKN/model/Dealer.scala
+++ src/main/scala/de/htwg/se/blackjackKN/model/Dealer.scala
@@ -1,5 +1,20 @@
 package de.htwg.se.blackjackKN.model
 
-case class Dealer(name: String) extends Person {
+import scala.collection.mutable.ListBuffer
+
+case class Dealer(name: String = "Dealer") extends Person {
+  var availCards : List[Card] = CardDeck().cardDeck
+  var buffer : ListBuffer[Card] = ListBuffer.empty[Card]
+
+
+  def generateDealerCards: List[Card] = {
+    for {
+      i <- 1 to 5
+    } availCards ++= CardDeck().cardDeck
+    availCards.copyToBuffer(buffer)
+    availCards
+  }
+
+
   override def toString: String = name
 }
