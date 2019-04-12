a8682963764b41d6c36e93bea253e21c7a018202
jasieben <jana.sieben@aol.de>
2019-04-12 12:07:06 +0200

    New case in Tui.scala

--- src/main/scala/de/htwg/se/blackjackKN/aview/Tui.scala
+++ src/main/scala/de/htwg/se/blackjackKN/aview/Tui.scala
@@ -8,6 +8,8 @@ class Tui {
       case "q" =>
         println("Exiting Blackjack...")
         System.exit(0)
+      case "Scala ist toll!" =>
+        println("Sag mir was neues\n (☞ﾟヮﾟ)☞ ")
       case _ =>
         println("Input not recognized!")
     }
