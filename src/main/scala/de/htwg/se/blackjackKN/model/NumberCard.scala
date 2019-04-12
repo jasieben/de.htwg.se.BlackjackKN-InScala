595356cd38cde7529dd0c791f547fa98f627e3d6
Benjamin Jasper <jasper.benni@gmail.com>
2019-04-12 13:51:22 +0200

    fixed numcards (2-10)

--- src/main/scala/de/htwg/se/blackjackKN/model/NumberCard.scala
+++ src/main/scala/de/htwg/se/blackjackKN/model/NumberCard.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.blackjackKN.model
 
 case class NumberCard() {
-  private val nums = List (1,2,3, 4, 5, 6, 7, 8, 9, 10)
+  private val nums = List (2, 3, 4, 5, 6, 7, 8, 9, 10)
   private val suits = List("hearts", "spades", "pikes", "clovers")
   val numcards: List[(Int, String)] = for {
     n <- nums
