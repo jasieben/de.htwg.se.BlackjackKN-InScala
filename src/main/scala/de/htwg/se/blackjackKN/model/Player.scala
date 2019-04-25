40870529dc1138116b6658a909e93759e08b1824
benjasper <jasper.benni@gmail.com>
2019-04-25 14:09:33 +0200

    implemented some basic PlayerHand methods

--- src/main/scala/de/htwg/se/blackjackKN/model/Player.scala
+++ src/main/scala/de/htwg/se/blackjackKN/model/Player.scala
@@ -1,5 +1,22 @@
 package de.htwg.se.blackjackKN.model
 
+import scala.collection.mutable.ListBuffer
+
 case class Player(name: String, balance: Int = 0) extends Person{
+
+  private var playerHand : ListBuffer[Card] = ListBuffer.empty
+
+  def addCardtoHand(card: Card): Card = {
+    playerHand += card
+    card
+  }
+
+  def clearHand() : Unit = {
+    playerHand.clear()
+  }
+
+  def getCard(index : Int): Card = {
+    playerHand(index)
+  }
   override def toString:String = name
 }
