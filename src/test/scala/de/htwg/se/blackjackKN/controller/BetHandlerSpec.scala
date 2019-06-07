82c8fcea9a6f05335a548776a60f20355e62250d
jasieben <jana.sieben@aol.de>
2019-06-07 10:39:35 +0200

    BetHandlerSpec tests for BetHandler, BWLHandler

---
+++ src/test/scala/de/htwg/se/blackjackKN/controller/BetHandlerSpec.scala
@@ -0,0 +1,25 @@
+package de.htwg.se.blackjackKN.controller
+
+import de.htwg.se.blackjackKN.controller.{Controller, GameState}
+import de.htwg.se.blackjackKN.controller.BetHandler
+import de.htwg.se.blackjackKN.model.{Bet, Player}
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class BetHandlerSpec extends WordSpec with Matchers{
+"A BetHandler " when {
+    val win = new WinningHandler(null)
+    val loose = new LoosingHandler(win)
+    val blackjack = new BlackjackHandler(loose)
+    val push = new PushHandler(blackjack)
+  "handle Winning Request" in {
+    val player = Player()
+    player.bet = Bet(100)
+    push.handleRequest(GameState.PLAYER_WINS, player)
+    player.balance should be (1200)
+  }
+}
+}
