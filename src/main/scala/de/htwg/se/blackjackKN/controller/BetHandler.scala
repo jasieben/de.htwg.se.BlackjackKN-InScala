9657d05cbe6d69d35a7e15e7842aa85375e0bee3
jasieben <jana.sieben@aol.de>
2019-06-21 10:19:13 +0200

    Interface for Controller

--- src/main/scala/de/htwg/se/blackjackKN/controller/BetHandler.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.blackjackKN.controller
-
-import de.htwg.se.blackjackKN.controller.GameState.GameState
-import de.htwg.se.blackjackKN.model.personsComponent.personsBaseImpl.Player
-
-trait BetHandler {
-  def handleRequest(state : GameState, player: Player): Unit = {}
-  val successor: BetHandler
-}
