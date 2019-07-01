35f14eff2eb831a6352230185120d73816c0105f
Benjamin Jasper <jasper.benni@gmail.com>
2019-07-01 20:39:29 +0200

    added some tests

---
+++ src/test/scala/de/htwg/se/blackjackKN/util/CommandSpec.scala
@@ -0,0 +1,36 @@
+package de.htwg.se.blackjackKN.util
+
+import org.scalatest.{Matchers, WordSpec}
+
+class CommandSpec extends WordSpec with Matchers {
+  "A Command" should {
+
+    "have a do step" in {
+      val command = new incrCommand
+      command.state should be(0)
+      command.doStep
+      command.state should be(1)
+      command.doStep
+      command.state should be(2)
+
+    }
+    "have an undo step" in {
+      val command = new incrCommand
+      command.state should be(0)
+      command.doStep
+      command.state should be(1)
+      command.undoStep
+      command.state should be(0)
+    }
+    "have a redo step" in {
+      val command = new incrCommand
+      command.state should be(0)
+      command.doStep
+      command.state should be(1)
+      command.undoStep
+      command.state should be(0)
+      command.redoStep
+      command.state should be(1)
+    }
+  }
+}
